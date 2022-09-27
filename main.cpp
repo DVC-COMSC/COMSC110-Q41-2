d307722672a5c1d8eb0a7128002f483dc06fe827
Kyuwoong Lee <kwooler@gmail.com>
2022-09-26 20:42:39 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,20 @@
-// Rewrite the following program. Use a switch statement instead of the if/else if statement.
-
+// Complete the following program. 
 #include <iostream>
 using namespace std;
-int main() 
-{
-        int selection;
-        cout << "Which formula do you want to see?\n\n";
-        cout << "1. Area of a circle\n";
-        cout << "2. Area of a rectangle\n";
-        cout << "3. Area of a cylinder\n" ;
-        cout << "4. None of them!\n";
-        cin >> selection;
-//*        if (selection == 1)
-//*                cout << "Pi times radius squared\n";
-//*        else if (selection == 2)
-//*                cout << "Length times width\n";
-//*        else if (selection == 3)
-//*                cout << "Pi times radius squared times height\n";
-//*        else if (selection == 4)
-//*                cout << "Well okay then, good bye!\n";
-//*        else
-//*                cout < < < < "Not good with numbers, eh?\n";
 
+int main()
+{
+	const double rate1 = 1.10;
+	const double rate2 = 2.20;
+	const double rate3 = 3.70;
+	const double rate4 = 4.80;
+	double total_charge;
+	double weight, distance, rate;
+
+	
+        // **********************
+        // Your code 
+        // **********************
+        
+	cout << total_charge << endl;
 }
